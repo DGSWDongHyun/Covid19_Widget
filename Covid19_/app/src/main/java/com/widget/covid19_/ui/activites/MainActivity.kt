05cf4e31e480877cca76da8185714cdb51323be8
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2021-02-07 23:03:23 +0900

    [MODIFY] ready to version 1

--- Covid19_/app/src/main/java/com/widget/covid19_/ui/activites/MainActivity.kt
+++ Covid19_/app/src/main/java/com/widget/covid19_/ui/activites/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.widget.covid19_.ui.activites
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -7,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AlertDialog
 import com.widget.covid19_.R
 import com.widget.covid19_.ui.activites.intro.IntroActivity
 
@@ -22,5 +24,17 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "차후 공개될 예정입니다.", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onBackPressed() {
+        AlertDialog.Builder(this)
+            .setTitle("종료하시겠습니까?")
+            .setMessage("앱을 종료하더라도 위젯으로 코로나 현황을 확인 할 수 있습니다.")
+            .setPositiveButton("네") { dialog: DialogInterface?, which: Int ->
+                finish()
+            }
+            .setNegativeButton("아니오") { dialog: DialogInterface?, which: Int ->
+
+            }.show()
     }
 }
--- Covid19_/app/src/main/java/com/widget/covid19_/ui/activites/intro/IntroActivity.kt
+++ Covid19_/app/src/main/java/com/widget/covid19_/ui/activites/intro/IntroActivity.kt
@@ -1,17 +1,60 @@
 package com.widget.covid19_.ui.activites.intro
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color.green
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.rubengees.introduction.IntroductionBuilder
+import com.rubengees.introduction.Slide
 import com.widget.covid19_.R
+import com.widget.covid19_.ui.activites.intro.slider.GuideSlide
+
 
 class IntroActivity : AppCompatActivity() {
+    var prefs : SharedPreferences ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
+        prefs = getSharedPreferences("Pref", MODE_PRIVATE);
+
         Handler().postDelayed(Runnable {
+            if(checkFirstRun())
+                IntroductionBuilder(this).withSlides(generateSlides()!!).introduceMyself();
             finish()
+            IntroductionBuilder(this).withSlides(generateSlides()!!).introduceMyself();
         }, 2500)
+    }
+    fun checkFirstRun() : Boolean {
+        val isFirstRun: Boolean = prefs!!.getBoolean("isFirstRun", true)
+        if (isFirstRun) {
+            prefs?.edit()?.putBoolean("isFirstRun", false)?.apply()
+            return true
+        }
+        return false
+    }
+
+    private fun generateSlides(): List<Slide>? {
+        val result: MutableList<Slide> = ArrayList()
+        result.add(Slide()
+                .withTitleSize(18F)
+                .withTitle("코로나 진행 상황을 그때 그때마다 확인하시나요?")
+                .withDescription("코로나 상황을 매번 앱으로 혹은 웹으로 접속하는 건 번거롭죠.\n 그렇다면 휴대폰을 키면 바로 확인 가능한건 어떠신가요?")
+                .withColorResource(R.color.gray)
+                .withDescriptionSize(14F)
+        )
+        result.add(Slide()
+                .withTitleSize(18F)
+                .withDescriptionSize(14F)
+                .withTitle("이제 편하게 이용하세요!")
+                .withDescription("홈 화면에서 간단하게 위젯을 추가해서, 확인해봐요!")
+                .withColorResource(R.color.gray)
+        )
+        return result
+    }
+    override fun onBackPressed() {
+        return;
     }
 }
